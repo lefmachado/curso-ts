7c1867addf522fb1fcfa92cf8d43e1504215b6b0
Leandro Machado <lefmachado@gmail.com>
2023-12-02 20:34:56 -0300

    Meu segundo Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return "Ol\u00E1 ".concat(name);
+}
+console.log(greet('Luiz Otávio'));
+var nome = 'Luiz';
+exports.default = nome;
---
+++ index.ts
@@ -0,0 +1,9 @@
+function greet(name: string) {
+    return `Olá ${name}`;
+}
+
+console.log(greet('Luiz Otávio'));
+
+const nome = 'Luiz';
+
+export default nome;
